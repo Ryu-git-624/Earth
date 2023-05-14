b5f059866301a50e5cd8b508c444dd86bfd892e9
竜也 秋山 <fallmountain624@outlook.jp>
2023-05-14 15:24:16 +0900

    メッセージの編集

--- Earth/Form1.cs
+++ Earth/Form1.cs
@@ -9,7 +9,7 @@ namespace Earth
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("AAA");
+            MessageBox.Show("AAABB");
         }
     }
 }
